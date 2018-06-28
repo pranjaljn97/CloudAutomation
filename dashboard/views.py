b5e3818ec0458abe644487ba781f1e4069fdd631
Mudit Mehta <muditbudy@gmail.com>
2018-06-28 13:40:41 +0530

    url changed

--- dashboard/views.py
+++ dashboard/views.py
@@ -37,10 +37,11 @@ def thanks(request):
 @login_required(login_url='/login/')
 def cloudprovider(request):
     uname = request.user.get_username()
-   
+    if uname == 'admin':
 #   today = datetime.datetime.now().date()
-    return render(request, "dashboard/cloud-provider.html")
-    
+        return render(request, "dashboard/cloud-provider.html")
+    else:
+        return render(request, "dashboard/noauth.html")
 
 @login_required(login_url='/login/')
 def userdata(request):
@@ -56,38 +57,37 @@ def forapproval(request):
      posts = Project.objects.all()
      return render(request, "dashboard/forapproval.html", {'posts': posts })
 
-def approvedsuccessfully(request, id):
+def forapproval1(request, id):
     
     currpost = Project.objects.get(id=id)
     currpost.status = 'Approved'
     currpost.save()
     posts = Project.objects.all()
+
     #mail functionality
-    umail = request.user.email
     subject = "Approval Request"
     message = "Congratulations your stack request has been approved"
     from_email = settings.EMAIL_HOST_USER
-    to_list = [umail]
+    to_list = ["mehtamudit1804@gmail.com"]
     send_mail(subject, message, from_email, to_list, fail_silently=False)
 
    # posts.status = 'Approved'
     #posts.save()
     return render(request, "dashboard/forapproval.html", {'posts': posts })
 
-def rejectedsuccessfully(request, id):
+def forapproval2(request, id):
     
     currpost = Project.objects.get(id=id)
     currpost.status = 'Rejected'
     currpost.save()
     posts = Project.objects.all()
-    #mail functionality
-    umail = request.user.email
+
+     #mail functionality
     subject = "Approval Request"
     message = "Sorry your stack request has been rejected"
     from_email = settings.EMAIL_HOST_USER
-    to_list = [umail]
+    to_list = ["mehtamudit1804@gmail.com"]
     send_mail(subject, message, from_email, to_list, fail_silently=False)
-
     #posts.status = newstatus
     #posts = Project.objects.get(pk=id)
     #posts.status = 'Rejected'
@@ -99,26 +99,6 @@ def rejectedsuccessfully(request, id):
 
 
 
-
-
-@login_required(login_url='/login/')
-def detailform(request,id):
-#   today = datetime.datetime.now().date()
-     uname = request.user.get_username()
-     posts = Project.objects.get(pk=id)
-     return render(request, "dashboard/detailform.html", {'posts': posts })
-@login_required(login_url='/login/')
-def approved(request):
-#   today = datetime.datetime.now().date()
-     uname = request.user.get_username()
-     posts = Project.objects.all()
-     return render(request, "dashboard/approved.html", {'posts': posts })
-@login_required(login_url='/login/')
-def rejected(request):
-#   today = datetime.datetime.now().date()
-     uname = request.user.get_username()
-     posts = Project.objects.all()
-     return render(request, "dashboard/rejected.html", {'posts': posts })
 
 
 
@@ -143,17 +123,7 @@ def drupalform(request):
         if form.is_valid():
             form.save()
             #pass
-            
-            #mail functionality
-            projectname = form.cleaned_data.get('project_name')
-            #currpost = Project.objects.filter(id=currid)
-            umail = request.user.email
-            subject = "Request Submitted"
-            message = "Your form has been submitted \n Please verify your details: \n" + "Project Name: " + projectname
-            from_email = settings.EMAIL_HOST_USER
-            to_list = [umail]
-            send_mail(subject, message, from_email, to_list, fail_silently=False)
-
+         
             return HttpResponseRedirect('/dashboard/')  # does nothing, just trigger the validation
         else:
             print(form.errors)   
