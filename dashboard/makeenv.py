bf382b9f2457006ef399841c0bbd90dd28c1c242
Mudit Mehta <muditbudy@gmail.com>
2018-07-02 14:52:11 +0530

    updated makeenv file

--- dashboard/makeenv.py
+++ dashboard/makeenv.py
@@ -4,6 +4,7 @@ import json
 import io
 import MySQLdb
 import sys
+from dashboard.models import Project
 
 def makeenvfile(myid):
     connection = MySQLdb.connect (host = "localhost", user = "root", passwd = "mehta123", db = "myproject")
@@ -16,52 +17,85 @@ def makeenvfile(myid):
     except NameError:
         to_unicode = str
     for row in data :
+        post = Project.objects.get(pk=myid)
  
-        data = {'user': {'id': row[0],
-                         'USERNAME': row[1],
-                         'project_name':row[4],
-			             'application_name':row[5],
-			             'git_url':row[6],
-			             'envtype':row[3],
-		       	         'platform':row[2]},
+        data = {'user': {'id': post.id,
+                         'USERNAME': post.requester,
+                         'project_name': post.project_name,
+			             'application_name': post.application_name,
+			             'git_url': post.git_url,
+			             'envtype': post.envtype,
+		       	         'platform': post.platform},
 
 	            'nginx_php': { 'enable': True,
-                              'PHP_VERSION':row[9],
-			                 'PHP_MODULES':row[10],
-			                 'NGINX_BACKEND_HOST_VALUE':row[11],
-                             'NGINX_SERVER_NAME_VALUE': row[12],
-                            'NGINX_SERVER_ROOT_VALUE':row[13],
-                            'NGINX_STATIC_CONTENT_ACCESS_LOG_VALUE':row[14],                      
-                            'NGINX_STATIC_CONTENT_EXPIRES_VALUE':row[15],
-                            'NGINX_DRUPAL_FILE_PROXY_URL_VALUE':row[16]},
-	            'mysql' : {  'enable': True,
-                            'mysql_version':row[22],
- 		       	            'MYSQL_CLIENT_DEFAULT_CHARACTER_SET_VALUE':row[28],
-                            'MYSQL_DATABASE':row[23],                                     
-                            'MYSQL_DUMP_MAX_ALLOWED_PACKET':row[29],
-                            'MYSQL_PASSWORD':row[26],            
-                            'MYSQL_PORT_VALUE':row[27],
-			                'MYSQL_ROOT_PASSWORD':row[24],
-			                'MYSQL_USER':row[25]},
+                               'env': {
+                                'PHP_VERSION': post.PHP_VERSION,
+			                    'PHP_MODULES': post.PHP_MODULES,
+			                    'NGINX_BACKEND_HOST_VALUE': post.NGINX_BACKEND_HOST_VALUE,
+                                'NGINX_SERVER_NAME_VALUE': post.NGINX_SERVER_NAME_VALUE,
+                                'NGINX_SERVER_ROOT_VALUE': post.NGINX_SERVER_ROOT_VALUE,
+                                'NGINX_STATIC_CONTENT_ACCESS_LOG_VALUE': post.NGINX_STATIC_CONTENT_ACCESS_LOG_VALUE,                      
+                                'NGINX_STATIC_CONTENT_EXPIRES_VALUE': post.NGINX_STATIC_CONTENT_EXPIRES_VALUE,
+                                'NGINX_DRUPAL_FILE_PROXY_URL_VALUE': post.NGINX_DRUPAL_FILE_PROXY_URL_VALUE},
+
+                                'volumes': {
+
+                                }
+                                },
+	            'mysql' : { 'enable': True,
+                            'env': {
+                                'mysql_version': post.mysql_version,
+ 		       	                'MYSQL_CLIENT_DEFAULT_CHARACTER_SET_VALUE': post.MYSQL_CLIENT_DEFAULT_CHARACTER_SET_VALUE,
+                                'MYSQL_DATABASE': post.MYSQL_DATABASE_NAME_VALUE,                                     
+                                'MYSQL_DUMP_MAX_ALLOWED_PACKET': post.MYSQL_DUMP_MAX_ALLOWED_PACKET,
+                                'MYSQL_PASSWORD': post.MYSQL_PASSWORD_VALUE,            
+                                'MYSQL_PORT_VALUE': post.MYSQL_PORT_VALUE,
+			                    'MYSQL_ROOT_PASSWORD': post.MYSQL_ROOT_PASSWORD_VALUE,
+			                    'MYSQL_USER': post.MYSQL_USER_NAME_VALUE},
+                                'volumes': {
+                                    
+                                }
+                                },
+                           
                 "mongodb": { 'enable': True,
-         		             'MONGO_PORT_VALUE': row[17],
-			                 'MONGO_INITDB_DATABASE_VALUE': row[18],
-       			             'MONGO_INITDB_ROOT_USERNAME': row[19],
-        		             'MONGO_INITDB_ROOT_PASSWORD': row[20],
-			                'mongo_version': row[21],},
-	            'varnish' : {
+                              'env': {
+                                'MONGO_PORT_VALUE': post.MONGO_PORT_VALUE,
+			                    'MONGO_INITDB_DATABASE_VALUE': post.MONGO_INITDB_DATABASE_VALUE,
+       			                'MONGO_INITDB_ROOT_USERNAME': post.MONGO_INITDB_ROOT_USERNAME_VALUE,
+        		                'MONGO_INITDB_ROOT_PASSWORD': post.MONGO_INITDB_ROOT_PASSWORD_VALUE,
+			                    'mongo_version': post.mongo_version},
+                                'volumes': {
+                                    
+                                }
+                            },
+                            
+         		'varnish' : {
                              'enable': True,
-                             'VARNISH_BACKEND_HOST':row[31],
-                             'VARNISH_BACKEND_PORT':row[32],
-                             'VARNISH_PORT_VALUE':row[33],
-			                 'varnish_version':row[30]},
+                             'env': {
+                                'VARNISH_BACKEND_HOST': post.VARNISH_BACKEND_HOST_VALUE,
+                                'VARNISH_BACKEND_PORT': post.VARNISH_BACKEND_PORT_VALUE,
+                                'VARNISH_PORT_VALUE': post.VARNISH_PORT_VALUE,
+			                    'varnish_version': post.varnish_version},
+                                'volumes': {
+                                    
+                                }
+                            },
+
+                             
 	            'redis' : {
                             'enable': True,
-			                'REDIS_PASSWORD':row[35],
-			                'redis_version':row[34], }}
+                             'env': {
+                                'REDIS_PASSWORD': post.REDIS_PASSWORD_VALUE,
+			                    'redis_version': post.redis_version },
+                             'volumes': {
+                                    
+                                }
+                            }}
+
+			                
 
 # Write JSON file
-        with io.open('data'+str(row[0])+'.json', 'w', encoding='utf8') as outfile:
+        with io.open('data'+str(post.id)+'.json', 'w', encoding='utf8') as outfile:
             str_ = json.dumps(data,
                       indent=4, sort_keys=True,
                       separators=(',', ': '), ensure_ascii=False)
