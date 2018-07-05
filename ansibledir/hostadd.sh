0296c3b7767868c7e1b16720baaeb57d6bffdae3
Mudit Mehta <muditbudy@gmail.com>
2018-07-05 18:28:37 +0530

    host status updated

--- ansibledir/hostadd.sh
+++ ansibledir/hostadd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 playbook=$1
 envfile=$2
-echo $envfile
-sudo ansible-playbook $playbook --extra-vars "$envfile"
+directoryPath=$3
+sudo ansible-playbook $playbook --extra-vars "$envfile" > $directoryPath
+
+
