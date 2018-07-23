380174b49411f2e5f2223a3ed4ac8b5681811213
Ubuntu <ubuntu@ip-172-31-43-207.ec2.internal>
2018-07-23 05:30:35 +0000

    Fixing permission and file name change

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 from .models import Project
+from .models import Ports
+from .models import runningstack
+from .models import Host
+from .models import mysqluser
 
 admin.site.register(Project)
+admin.site.register(Ports)
+admin.site.register(runningstack)
+admin.site.register(Host)
+admin.site.register(mysqluser)
+
