6f7585966d5f4b05579b909f4f63b4f7c2e1cc4b
dipesh017 <dipeshgarg017@gmail.com>
2018-07-23 19:45:01 +0530

    renaming values in host

--- dashboard/exportProject.py
+++ dashboard/exportProject.py
@@ -4,9 +4,10 @@ import tarfile
 paramiko.util.log_to_file('/tmp/paramiko.log')
 paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
 
-HOST = 'local'
-USER = 22
-PASS = 'user'
+HOST = '34.224.87.40'
+USER = 's2portal'
+PASS = 'Ttn@1234'
+
 
 remote_images_path = '/remote_path/images/'
 local_path = '/tmp/'
@@ -20,6 +21,13 @@ try:
 except:
     print "SSH failed"
 sftp = client.open_sftp()
+
+
+with tarfile.open("sample.tar.gz", "w:gz") as tar_handle:
+        for root, dirs, files in os.walk(/home/project_name):
+            for file in files:
+                tar_handle.add(os.path.join(root, file))
+tardir('./my_folder', 'sample.tar.gz')
 
 for file in files:
     file_remote = remote_images_path + file
