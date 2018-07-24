68483dd37959425c4d7d6dd6f07be71929bcedfd
Ubuntu <ubuntu@ip-172-31-43-207.ec2.internal>
2018-07-24 10:39:22 +0000

    merge conflict fixed

--- dashboard/views.py
+++ dashboard/views.py
@@ -235,18 +235,18 @@ def approvedsuccessfully(request, id):
     jsonfile = currpost.project_name
     appname = currpost.application_name
     hostip = currpost.hostIp
-    try:
-	 buildinfo(request,id,jsonfile,hostip)
-    except:
-         msg = "Error in fetching final status"
-         return render(request, "dashboard/error.html", {'msg': msg })
+    #try:
+    buildinfo(request,id,jsonfile,hostip)
+    #except:
+     #    msg = "Error in fetching final status"
+      #   return render(request, "dashboard/error.html", {'msg': msg })
   
-    try:
-         add_cname_record(request,id,jsonfile,appname,hostip)
+    #try:
+    add_cname_record(request,id,jsonfile,appname,hostip)
     
-    except:
-         msg = "Error in adding A record in AWS Route53"
-         return render(request, "dashboard/error.html", {'msg': msg })
+   # except:
+    #     msg = "Error in adding A record in AWS Route53"
+     #    return render(request, "dashboard/error.html", {'msg': msg })
     currpost.approvedBy = request.user.email
     currpost.status = 'Approved'
     currpost.save()
